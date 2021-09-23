d697b2e3a9554641afc5d0b0599fcb49a3c77af2
nwnp-desktop <jjw_0418@naver.com>
2021-09-24 03:40:24 +0900

    study: ch05. ExpressionAndStatement

--- Ch05.ExpressionAndStatement/05-01.js
+++ Ch05.ExpressionAndStatement/05-01.js
@@ -1,3 +1,21 @@
 // @ts-check
 
 /* eslint-disable no-console */
+// 값(value)은 식(expression)이 평가(evaluate)되어 생성된 결과를 말한다
+var sum = 10 + 20;
+console.log(sum);
+
+// 리터럴은 사람이 이해할 수 있는 문자 또는 약속된 기호를 사용해 값을 생성하는 표기법(notation)을 말한다.
+// 정수, 부동소수점, 2진수, 8진수, 16진수, 문자열, 불리언, null, undefined, 객체, 배열, 함수, 정규표현식 리터럴
+
+function foo() {}
+
+var x = 3;
+
+if (x > 1) {
+  console.log(x);
+}
+
+for (var i = 0; i < 2; i++) {
+  console.log(i);
+}
